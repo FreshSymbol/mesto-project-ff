6fa91b2ca54669a4d0d9b3f309c0a45ab8c832bb
FreshSymbol <m4developer@yandex.ru>
2024-01-03 18:25:32 +0300

    removed unnecessary functionality

--- scripts/index.js
+++ scripts/index.js
@@ -4,47 +4,6 @@ const cardItem = templateCard.querySelector(".places__item");
 
 // @todo: DOM узлы
 const placeList = document.querySelector(".places__list");
-const addButton = document.querySelector(".profile__add-button");
-const modalNewCard = document.querySelector(".popup_type_new-card");
-const modalNewCardCloseButton = modalNewCard.querySelector(".popup__close");
-const formNewCard = modalNewCard.querySelector(".popup__form");
-const inputCardTitle = formNewCard.querySelector(
-  ".popup__input_type_card-name"
-);
-const inputCardImageUrl = formNewCard.querySelector(".popup__input_type_url");
-const modalImage = document.querySelector(".popup_type_image");
-const modalImageImg = modalImage.querySelector(".popup__image");
-const modalImageText = modalImage.querySelector(".popup__caption");
-const modalImageCloseButton = modalImage.querySelector(".popup__close");
-
-//Попап формы
-addButton.addEventListener("click", () =>
-  modalNewCard.classList.add("popup_is-opened")
-);
-modalNewCardCloseButton.onclick = () =>
-  modalNewCard.classList.remove("popup_is-opened");
-
-formNewCard.addEventListener("submit", (event) => {
-  event.preventDefault();
-  placeList.append(
-    createCard(
-      { name: inputCardTitle.value, link: inputCardImageUrl.value },
-      deleteCard
-    )
-  );
-  modalNewCard.classList.remove("popup_is-opened");
-  inputCardTitle.value = "";
-  inputCardImageUrl.value = "";
-});
-
-//Попап изображений
-function showModalImage(src, text) {
-  modalImage.classList.add("popup_is-opened");
-  modalImageImg.src = src;
-  modalImageText.textContent = text;
-  modalImageCloseButton.onclick = () =>
-    modalImage.classList.remove("popup_is-opened");
-}
 
 // @todo: Функция создания карточки
 function createCard(card, deleteCard) {
@@ -52,9 +11,6 @@ function createCard(card, deleteCard) {
   const cardImage = cardCopy.querySelector(".card__image");
   const cardTitle = cardCopy.querySelector(".card__title");
   const deleteButton = cardCopy.querySelector(".card__delete-button");
-  cardImage.addEventListener("click", () =>
-    showModalImage(cardImage.src, cardTitle.textContent)
-  );
   deleteButton.addEventListener("click", () => deleteCard(cardCopy));
   cardImage.src = card.link;
   cardTitle.textContent = card.name;
