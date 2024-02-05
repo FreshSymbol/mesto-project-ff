3021ab58a80b07669848addaaf4e35c055b63369
FreshSymbol <m4developer@yandex.ru>
2024-02-05 21:42:20 +0300

    rename functions

--- src/scripts/components/modal.js
+++ src/scripts/components/modal.js
@@ -4,21 +4,21 @@ modals.forEach((modal) => modal.classList.add("popup_is-animated"));
 function openModal(element) {
   element.classList.add("popup_is-opened");
   element.querySelector(".popup__close").focus();
-  element.addEventListener("keydown", modalCloseKeykHandler);
-  element.addEventListener("click", modalCloseClickHandler);
+  element.addEventListener("keydown", closeModalKeykHandler);
+  element.addEventListener("click", closeModalClickHandler);
 }
 
 function closeModal(element) {
   element.classList.remove("popup_is-opened");
-  element.removeEventListener("keydown", modalCloseKeykHandler);
-  element.removeEventListener("click", modalCloseClickHandler);
+  element.removeEventListener("keydown", closeModalKeykHandler);
+  element.removeEventListener("click", closeModalClickHandler);
 }
 
-function modalCloseKeykHandler(event) {
+function closeModalKeykHandler(event) {
   if (event.key === "Escape") closeModal(this);
 }
 
-function modalCloseClickHandler(event) {
+function closeModalClickHandler(event) {
   if (
     event.target.classList.contains("popup__close") ||
     event.target.classList.contains("popup")
